40ad0bb91815ee397a18c5fb7b5ad1d00cd449af
Xaver <xaver@everpress.co>
2024-05-27 15:18:22 +0200

    updated readme

--- mailster-mailjet.php
+++ mailster-mailjet.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: Mailster Mailjet
+Requires Plugins: mailster
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=Mailjet
 Description: Uses Mailjet to deliver emails for the Mailster Newsletter Plugin for WordPress.
 Version: 1.1.2
@@ -12,7 +13,7 @@ License: GPLv2 or later
 
 
 define( 'MAILSTER_MAILJET_VERSION', '1.1.2' );
-define( 'MAILSTER_MAILJET_REQUIRED_VERSION', '3.0' );
+define( 'MAILSTER_MAILJET_REQUIRED_VERSION', '4.0' );
 define( 'MAILSTER_MAILJET_FILE', __FILE__ );
 
 require_once __DIR__ . '/classes/mailjet.class.php';
